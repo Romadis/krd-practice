2e996815fb2c255433f4016e1bda16b9f62fcf2c
romadis <romadis2024@mail.ru>
2024-07-08 14:40:12 +0300

    Create file with methods greeting and foobar

--- lesson_1/main.rb
+++ lesson_1/main.rb
@@ -6,13 +6,13 @@ def greeting
     surname = gets.chomp
 
     puts "Введите ваш возраст:"
-    age = gets.chomp.to_i
+    age = gets.to_i
 
     if age < 18
-        puts "Привет, #{name} #{surname}. Тебе меньше 18 лет, но начать учиться программировать никогда не рано"
+        "Привет, #{name} #{surname}. Тебе меньше 18 лет, но начать учиться программировать никогда не рано"
     
     else
-        puts "Привет, #{name} #{surname}. Самое время заняться делом"
+        "Привет, #{name} #{surname}. Самое время заняться делом"
     end
 end
 
